a522bfae0a0863c6d3d85fdb4ee04a9c9075e4b4
MWagersUCA <98266137+MWagersUCA@users.noreply.github.com>
2022-10-09 07:44:17 -0500

    cleaned up code w/ comments added

--- Main.py
+++ Main.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
+# =============================================================================
 # Algorithms project 2
 # Due date: October 9, 2022
 # Authors: Matt Wagers, ..., ...
+# =============================================================================
+# This code was written and created to test the time complexity and
+#   sorting computation time of 4 different sorting algorithms.
+# =============================================================================
+# Arrays of lengths: 100, 1_000, 10_000, 100_000
+# Algorithms tested: Bubble, Merge, Quick, Insertion
+# =============================================================================
+# Arrays were tested with unique values in random order
+#   then tested against an already sorted array (least to greatest)
+#   finally tested against a reversed ordered array (greatest to least)
+# =============================================================================
 
 import time
 import random 
-import sys
-import pandas as pd # Could use for graphs
-
-sys.setrecursionlimit(1_000_000)
 
 # Generates random numbers
+# This was used to test arrays with possible duplicate values
 # =============================================================================
 # def randomNumberList(num, start, end):
 #     result = []
@@ -21,6 +30,8 @@ sys.setrecursionlimit(1_000_000)
 #     return result
 # =============================================================================
 
+# Function to create an array with unique numbers in each element
+# random.shuffle() used to randomize the locations of each value
 def randomNumberList(num, start, end):
     result = []
     for i in range(num):
@@ -28,6 +39,7 @@ def randomNumberList(num, start, end):
         
     random.shuffle(result)
     return result
+# End of Random Number List
 
 # Code for Bubble Sort
 def bubble_sort(arr):
@@ -46,7 +58,7 @@ def bubble_sort(arr):
                 swap(i - 1, i)
                 swapped = True  
     return arr
-
+# End of Bubble Sort
 
 # Code for Merge Sort
 def merge_sort(arr):
@@ -76,28 +88,96 @@ def merge(left, right, merged):
         merged[left_cursor + right_cursor] = right[right_cursor]
         
     return merged
-
+# End of Merge Sort
 
 # Code for Quick Sort
-def partition(array, begin, end):
-    pivot_idx = begin
-    for i in range(begin + 1, end + 1):
-        if array[i] <= array[begin]:
-            pivot_idx += 1
-            array[i], array[pivot_idx] = array[pivot_idx], array[i]
-    array[pivot_idx], array[begin] = array[begin], array[pivot_idx]
-    return pivot_idx
-def quick_sort_recursion(array, begin, end):
-    if begin >= end:
-        return
-    pivot_idx = partition(array, begin, end)
-    quick_sort_recursion(array, begin, pivot_idx - 1)
-    quick_sort_recursion(array, pivot_idx + 1, end)
-def quick_sort(array, begin = 0, end = None):
-    if end is None:
-        end = len(array) - 1
-    return quick_sort_recursion(array, begin, end)
+# Failed to run correctly with array of 10,000 elements or more
+# =============================================================================
+# def partition(array, begin, end):
+#     pivot_idx = begin
+#     for i in range(begin + 1, end + 1):
+#         if array[i] <= array[begin]:
+#             pivot_idx += 1
+#             array[i], array[pivot_idx] = array[pivot_idx], array[i]
+#     array[pivot_idx], array[begin] = array[begin], array[pivot_idx]
+#     return pivot_idx
+# def quick_sort_recursion(array, begin, end):
+#     if begin >= end:
+#         return
+#     pivot_idx = partition(array, begin, end)
+#     quick_sort_recursion(array, begin, pivot_idx - 1)
+#     quick_sort_recursion(array, pivot_idx + 1, end)
+# def quick_sort(array, begin = 0, end = None):
+#     if end is None:
+#         end = len(array)  - 1
+#     return quick_sort_recursion(array, begin, end)
+# =============================================================================
 
+# Secondary code for Quick Sort
+# Similar issue to the previous Quick Sort Algorithm
+# =============================================================================
+# def partition(array, low, high):
+#     pivot = array[high]
+#     i = low - 1
+#     for j in range(low, high):
+#         if array[j] <= pivot:
+#             i = i + 1
+#             (array[i], array[j]) = (array[j], array[i])
+#     (array[i + 1], array[high]) = (array[high], array[i + 1])
+#     return i + 1
+# 
+# def quick_sort(array, low, high):
+#     if low < high:
+#         pi = partition(array, low, high)
+#         quick_sort(array, low, pi - 1)
+#         quick_sort(array, pi + 1, high)
+# =============================================================================
+
+# This version of Quick Sort yielded no Stack Overflow errors
+# Iterative and non-recursive
+# This code was found at (https://stackoverflow.com/questions/68524038/is-there-a-python-implementation-of-quicksort-without-recursion)
+def partition(arr, l, h):
+    i = (l - 1)
+    x = arr[h]
+    
+    for j in range(l, h):
+        if arr[j] <= x:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[h] = arr[h], arr[i + 1]
+    return (i + 1)
+
+def quick_sort(arr, l, h):
+    size = h - l + 1
+    stack = [0] * (size)
+    
+    top = -1
+    
+    top = top + 1
+    stack[top] = l
+    top = top + 1
+    stack[top] = h
+    
+    while top >= 0:
+        h = stack[top]
+        top = top - 1
+        l = stack[top]
+        top = top - 1
+        
+        p = partition(arr, l, h)
+        
+        if p - 1 > l:
+            top = top + 1
+            stack[top] = l
+            top = top + 1
+            stack[top] = p - 1
+        
+        if p + 1 < h:
+            top = top + 1
+            stack[top] = p + 1
+            top = top + 1
+            stack[top] = h
+# End of Quick Sort Algorithm
 
 # Code for Insertion Sort
 def insertion_sort(arr):
@@ -113,9 +193,10 @@ def insertion_sort(arr):
         arr[pos] = cursor
         
     return arr
-
+# End of Insertion Sort
 
 # Reverse order to test worst case for sorts
+# Sorts array by greatest to least
 def reverse_order(arr):
     for i in range(len(arr)):
         max = i
@@ -125,7 +206,7 @@ def reverse_order(arr):
                 
         arr[max], arr[i] = arr[i], arr[max]
     return arr
-
+# End of Reverse Order
 
 # Call this function by sending an array to test
 # along with a sorting algorithm to perform
@@ -136,16 +217,22 @@ def reverse_order(arr):
 #   quick_sort
 # Returns T(n)
 def experiment(arr, sort):
-    start_time = time.time()
-    sort(arr)
-    arr_total_time = time.time() - start_time
-    return arr_total_time
-
+    if sort == quick_sort:
+        start_time = time.time()
+        quick_sort(arr, 0, len(arr) - 1)
+        arr_total_time = time.time() - start_time
+        return arr_total_time
+    else:
+        start_time = time.time()
+        sort(arr)
+        arr_total_time = time.time() - start_time
+        return arr_total_time
+# End of Experiment Function
 
 
 # Testing Zone
 
-# Create Lists
+# Create Array Lists
 arrH = randomNumberList(100, 1, 100)
 arrT = randomNumberList(1000, 1, 1000)
 arrTT = randomNumberList(10_000, 1, 10_000)
@@ -153,98 +240,85 @@ arrHT = randomNumberList(100_000, 1, 100_000)
 
 
 # Test Buble Sort
-# =============================================================================
-# print("Array of 100 Elements: Bubble Sort")
-# print("Average T(n): ", experiment(arrH, bubble_sort))
-# print("Best T(n): ", experiment(arrH, bubble_sort))
-# print("Worst T(n): ", experiment(reverse_order(arrH), bubble_sort))
-# 
-# print("\nArray of 1000 Elements: Bubble Sort")
-# print("Average T(n): ", experiment(arrT, bubble_sort))
-# print("Best T(n): ", experiment(arrT, bubble_sort))
-# print("Worst T(n): ", experiment(reverse_order(arrT), bubble_sort))
-# 
-# print("\nArray of 10_000 Elements: Bubble Sort")
-# print("Average T(n): ", experiment(arrTT, bubble_sort))
-# print("Best T(n): ", experiment(arrTT, bubble_sort))
-# print("Worst T(n): ", experiment(reverse_order(arrTT), bubble_sort))
-# 
-# print("\nArray of 100_000 Elements: Bubble Sort")
-# print("Average T(n): ", experiment(arrHT, bubble_sort))
-# print("Best T(n): ", experiment(arrHT, bubble_sort))
-# print("Worst T(n): ", experiment(reverse_order(arrHT), bubble_sort))
-# 
-# 
-# # Test Insertion Sort
-# print("Array of 100 Elements: Insertion Sort")
-# print("Average T(n): ", experiment(arrH, insertion_sort))
-# print("Best T(n): ", experiment(arrH, insertion_sort))
-# print("Worst T(n): ", experiment(reverse_order(arrH), insertion_sort))
-# 
-# print("\nArray of 1000 Elements: Insertion Sort")
-# print("Average T(n): ", experiment(arrT, insertion_sort))
-# print("Best T(n): ", experiment(arrT, insertion_sort))
-# print("Worst T(n): ", experiment(reverse_order(arrT), insertion_sort))
-# 
-# print("\nArray of 10_000 Elements: Insertion Sort")
-# print("Average T(n): ", experiment(arrTT, insertion_sort))
-# print("Best T(n): ", experiment(arrTT, insertion_sort))
-# print("Worst T(n): ", experiment(reverse_order(arrTT), insertion_sort))
-# 
-# print("\nArray of 100_000 Elements: Insertion Sort")
-# print("Average T(n): ", experiment(arrHT, insertion_sort))
-# print("Best T(n): ", experiment(arrHT, insertion_sort))
-# print("Worst T(n): ", experiment(reverse_order(arrHT), insertion_sort))
-# 
-# 
-# # Test Merge Sort
-# print("Array of 100 Elements: Merge Sort")
-# print("Average T(n): ", experiment(arrH, merge_sort))
-# print("Best T(n): ", experiment(arrH, merge_sort))
-# print("Worst T(n): ", experiment(reverse_order(arrH), merge_sort))
-# 
-# print("\nArray of 1000 Elements: Merge Sort")
-# print("Average T(n): ", experiment(arrT, merge_sort))
-# print("Best T(n): ", experiment(arrT, merge_sort))
-# print("Worst T(n): ", experiment(reverse_order(arrT), merge_sort))
-# 
-# print("\nArray of 10_000 Elements: Merge Sort")
-# print("Average T(n): ", experiment(arrTT, merge_sort))
-# print("Best T(n): ", experiment(arrTT, merge_sort))
-# print("Worst T(n): ", experiment(reverse_order(arrTT), merge_sort))
-# 
-# print("\nArray of 100_000 Elements: Merge Sort")
-# print("Average T(n): ", experiment(arrHT, merge_sort))
-# print("Best T(n): ", experiment(arrHT, merge_sort))
-# print("Worst T(n): ", experiment(reverse_order(arrHT), merge_sort))
-# =============================================================================
+print("Array of 100 Elements: Bubble Sort")
+print("Average T(n): ", experiment(arrH, bubble_sort))
+print("Best T(n): ", experiment(arrH, bubble_sort))
+print("Worst T(n): ", experiment(reverse_order(arrH), bubble_sort))
 
+print("\nArray of 1000 Elements: Bubble Sort")
+print("Average T(n): ", experiment(arrT, bubble_sort))
+print("Best T(n): ", experiment(arrT, bubble_sort))
+print("Worst T(n): ", experiment(reverse_order(arrT), bubble_sort))
+
+print("\nArray of 10_000 Elements: Bubble Sort")
+print("Average T(n): ", experiment(arrTT, bubble_sort))
+print("Best T(n): ", experiment(arrTT, bubble_sort))
+print("Worst T(n): ", experiment(reverse_order(arrTT), bubble_sort))
+
+print("\nArray of 100_000 Elements: Bubble Sort")
+print("Average T(n): ", experiment(arrHT, bubble_sort))
+print("Best T(n): ", experiment(arrHT, bubble_sort))
+print("Worst T(n): ", experiment(reverse_order(arrHT), bubble_sort))
+
+# Test Insertion Sort
+print("Array of 100 Elements: Insertion Sort")
+print("Average T(n): ", experiment(arrH, insertion_sort))
+print("Best T(n): ", experiment(arrH, insertion_sort))
+print("Worst T(n): ", experiment(reverse_order(arrH), insertion_sort))
  
+print("\nArray of 1000 Elements: Insertion Sort")
+print("Average T(n): ", experiment(arrT, insertion_sort))
+print("Best T(n): ", experiment(arrT, insertion_sort))
+print("Worst T(n): ", experiment(reverse_order(arrT), insertion_sort))
+ 
+print("\nArray of 10_000 Elements: Insertion Sort")
+print("Average T(n): ", experiment(arrTT, insertion_sort))
+print("Best T(n): ", experiment(arrTT, insertion_sort))
+print("Worst T(n): ", experiment(reverse_order(arrTT), insertion_sort))
+
+print("\nArray of 100_000 Elements: Insertion Sort")
+print("Average T(n): ", experiment(arrHT, insertion_sort))
+print("Best T(n): ", experiment(arrHT, insertion_sort))
+print("Worst T(n): ", experiment(reverse_order(arrHT), insertion_sort))
+
+# Test Merge Sort
+print("Array of 100 Elements: Merge Sort")
+print("Average T(n): ", experiment(arrH, merge_sort))
+print("Best T(n): ", experiment(arrH, merge_sort))
+print("Worst T(n): ", experiment(reverse_order(arrH), merge_sort))
+ 
+print("\nArray of 1000 Elements: Merge Sort")
+print("Average T(n): ", experiment(arrT, merge_sort))
+print("Best T(n): ", experiment(arrT, merge_sort))
+print("Worst T(n): ", experiment(reverse_order(arrT), merge_sort))
+ 
+print("\nArray of 10_000 Elements: Merge Sort")
+print("Average T(n): ", experiment(arrTT, merge_sort))
+print("Best T(n): ", experiment(arrTT, merge_sort))
+print("Worst T(n): ", experiment(reverse_order(arrTT), merge_sort))
+ 
+print("\nArray of 100_000 Elements: Merge Sort")
+print("Average T(n): ", experiment(arrHT, merge_sort))
+print("Best T(n): ", experiment(arrHT, merge_sort))
+print("Worst T(n): ", experiment(reverse_order(arrHT), merge_sort))
+
 # Test Quick Sort
 print("Array of 100 Elements: Quick Sort")
 print("Average T(n): ", experiment(arrH, quick_sort))
 print("Best T(n): ", experiment(arrH, quick_sort))
 print("Worst T(n): ", experiment(reverse_order(arrH), quick_sort))
-
+  
 print("\nArray of 1000 Elements: Quick Sort")
 print("Average T(n): ", experiment(arrT, quick_sort))
 print("Best T(n): ", experiment(arrT, quick_sort))
 print("Worst T(n): ", experiment(reverse_order(arrT), quick_sort))
-
+  
 print("\nArray of 10_000 Elements: Quick Sort")
 print("Average T(n): ", experiment(arrTT, quick_sort))
 print("Best T(n): ", experiment(arrTT, quick_sort))
 print("Worst T(n): ", experiment(reverse_order(arrTT), quick_sort))
-
+  
 print("\nArray of 100_000 Elements: Quick Sort")
 print("Average T(n): ", experiment(arrHT, quick_sort))
 print("Best T(n): ", experiment(arrHT, quick_sort))
 print("Worst T(n): ", experiment(reverse_order(arrHT), quick_sort))
-
-
-
-
-
-
-
-
